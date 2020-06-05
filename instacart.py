17b942a0b498192190c8bf23972e92c468694e0d
KovousoglouG <65331575+KovousoglouG@users.noreply.github.com>
2020-06-05 17:35:57 +0300

    Update instacart.py

--- instacart.py
+++ instacart.py
@@ -845,7 +845,7 @@ gc.collect()
 # In[ ]:
 
 
-#param_grid = {'max_depth' : [5, 6], 'subsample' : [0.9, 1]}
+param_grid = {'max_depth' : [7, 8], 'min_child_weight' : [2, 1]}
 
 #‘max_depth’ : 6                Μέγιστο βάθος ενός δέντρου
 #‘subsample’ : 1.0              Ποσοστό δεδομένων σε κάθε δέντρο
@@ -859,28 +859,44 @@ gc.collect()
 # In[ ]:
 
 
-#xg = xgb.XGBClassifier(objective = 'binary:logistic', tree_method = 'exact', eval_metric = 'logloss',  num_boost_round = 10)
+xg = xgb.XGBClassifier(learning_rate =0.1,
+                       n_estimators=140,
+                       max_depth=5,
+                       min_child_weight=1,
+                       gamma=0, 
+                       subsample=0.8,
+                       colsample_bytree=0.8,
+                       objective= 'binary:logistic',
+                       nthread=4,
+                       scale_pos_weight=1,
+                       seed=27, 
+                       tree_method = 'gpu_hist', 
+                       eval_metric = 'logloss',
+                       num_boost_round = 10)
 
 
 # In[ ]:
 
 
-#grid_search = GridSearchCV(estimator = xg, param_grid = param_grid, cv = 3, verbose = 2, n_jobs = 2)
+grid_search = GridSearchCV(estimator = xg, param_grid = param_grid, cv = 3, verbose = 2, n_jobs = 2)
 
 
 # In[ ]:
 
 
-#xg.get_params()
+xg.get_params()
 
 
 # In[ ]:
 
 
-#grid_search.fit(data_train.drop('reordered', axis=1), data_train.reordered)
+grid_search.fit(data_train.drop('reordered', axis=1), data_train.reordered)
 
 
 # In[ ]:
+
+print('The best params are: ', grid_search.best_params_)
+print('The achieved score with these params is: ', grid_search.best_score_)
 
 
 #results_dictionary = grid_search.best_params_
@@ -906,20 +922,20 @@ gc.collect()
 # In[99]:
 
 
-dm_train = xgb.DMatrix(data = data_train.drop('reordered', axis=1), label = data_train.reordered)
-dm_test = xgb.DMatrix(data = data_test)
+#dm_train = xgb.DMatrix(data = data_train.drop('reordered', axis=1), label = data_train.reordered)
+#dm_test = xgb.DMatrix(data = data_test)
 
 
 # In[100]:
 
 
-params = {'objective': 'binary:logistic', 'tree_method':'exact', 'eval_metric' : 'error@0.21', 'alpha': 0.4, 'colsample_bytree': 0.3, 'gammma': 0.1, 'max_depth': 6, 'subsample_bytree': 0.3}
+#params = {'objective': 'binary:logistic', 'tree_method':'exact', 'eval_metric' : 'error@0.21', 'alpha': 0.4, 'colsample_bytree': 0.3, 'gammma': 0.1, 'max_depth': 6, 'subsample_bytree': 0.3}
 
 
 # In[101]:
 
 
-xg = xgb.train(dtrain = dm_train, params = params, num_boost_round = 10)
+#xg = xgb.train(dtrain = dm_train, params = params, num_boost_round = 10)
 
 
 # In[102]:
@@ -934,91 +950,7 @@ xg = xgb.train(dtrain = dm_train, params = params, num_boost_round = 10)
 # In[105]:
 
 
-test_pred = (xg.predict(dm_test) >= 0.21)
-test_pred[0:20]
+#test_pred = (xg.predict(dm_test) >= 0.21)
+#test_pred[0:20]
 
-
-# # 5. Prepare submission file
-
-# In[106]:
-
-
-data_test['prediction'] = test_pred
-data_test.head()
-
-
-# In[107]:
-
-
-final = data_test.reset_index()
-final = final[['product_id', 'user_id', 'prediction']]
-gc.collect()
-final.head()
-
-
-# In[108]:
-
-
-orders_test = orders.loc[orders.eval_set=='test',("user_id", "order_id") ]
-orders_test.head()
-
-
-# In[109]:
-
-
-final = final.merge(orders_test, on='user_id', how='left')
-final.head()
-
-
-# In[110]:
-
-
-final = final.drop('user_id', axis=1)
-final['product_id'] = final.product_id.astype(int)
-
-del orders, test_pred
-del orders_test, data_test
-gc.collect()
-
-final.head()
-
-
-# In[111]:
-
-
-d = dict()
-for row in final.itertuples():
-    if row.prediction== 1:
-        try:
-            d[row.order_id] += ' ' + str(row.product_id)
-        except:
-            d[row.order_id] = str(row.product_id)
-for order in final.order_id:
-    if order not in d:
-        d[order] = 'None'
-        
-gc.collect()
-
-
-# In[112]:
-
-
-sub = pd.DataFrame.from_dict(d, orient='index')
-
-sub.reset_index(inplace=True)
-sub.columns = ['order_id', 'products']
-
-sub.head()
-
-
-# In[113]:
-
-
-sub.shape[0]
-
-
-# In[117]:
-
-
-sub.to_csv('sub.csv', index=False)
 
